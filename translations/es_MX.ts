7e3cdb0e778b8bce498434e603691d35848881fb
Sebastian-byte <sebastianmontoya209@gmail.com>
2021-02-15 22:16:54 -0500

    Add Spanish translation

---
+++ translations/es_MX.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_MX" sourcelanguage="en_US">
+<context>
+    <name>Drive</name>
+    <message>
+        <location filename="../backend.cpp" line="24"/>
+        <source>%1/%2 MB used</source>
+        <comment>%1 - used space; %2 - total space</comment>
+        <translation>%1/%2 MB usados</translation>
+    </message>
+    <message>
+        <location filename="../backend.cpp" line="29"/>
+        <source>%1/%2 GB used</source>
+        <comment>%1 - used space; %2 - total space</comment>
+        <translation>%1/%2 GB usados</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="11"/>
+        <source>Disk Usage</source>
+        <translatorcomment>It should be &quot;Uso del Disco&quot;, but doesn&apos;t fit</translatorcomment>
+        <translation>Almacenamiento</translation>
+    </message>
+</context>
+</TS>
