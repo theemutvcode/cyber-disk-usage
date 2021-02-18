e75601b314d27c41f3ddb4c1a09ea63e14ad4b1a
theemutvcode <55019667+theemutvcode@users.noreply.github.com>
2021-02-18 15:14:47 +0100

    add german translation

---
+++ translations/de_DE.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="de_DE">
+<context>
+    <name>Drive</name>
+    <message>
+        <location filename="../backend.cpp" line="24"/>
+        <source>%1/%2 MB used</source>
+        <comment>%1 - used space; %2 - total space</comment>
+        <translation>%1 MB benutzt von %2</translation>
+    </message>
+    <message>
+        <location filename="../backend.cpp" line="29"/>
+        <source>%1/%2 GB used</source>
+        <comment>%1 - used space; %2 - total space</comment>
+        <translation>%1 GB genutzt von %2</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="11"/>
+        <source>Disk Usage</source>
+        <translation>Speicherbenutzung</translation>
+    </message>
+</context>
+</TS>
